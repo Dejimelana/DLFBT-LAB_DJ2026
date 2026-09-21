2892233526de06c835a86e9d2d55fdae1d4bf6de
Dejimelana <42736608+Dejimelana@users.noreply.github.com>
2026-09-21 18:05:51 +0200

    Update dlfbt_lab1.py

--- Lab1/dlfbt_lab1.py
+++ Lab1/dlfbt_lab1.py
@@ -193,9 +193,10 @@ class LogisticRegressionModel(LinearRegressionModel):
         return 1.0 / (1.0 + np.exp(-z))
 
     # --- TO-DO block: Overwrite the methods of the LinearRegressionModel class
-        z = np.dot(x, self.w) + self.b
-        y = LogisticRegressionModel.sigmoid(z)
-        return y
+        def predict(self,x):
+          z = np.dot(x, self.w) + self.b
+          y = LogisticRegressionModel.sigmoid(z)
+          return y
     # --- End of TO-DO block
 
     def get_loss(self, x, t):
